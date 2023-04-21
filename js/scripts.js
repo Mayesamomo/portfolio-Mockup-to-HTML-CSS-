9b3c484722195c71804d115165803e5e0d399469
Michael A. Momo <mayesamomo1997@gmail.com>
2023-04-21 18:40:33 -0400

    animation added

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     let menu = document.querySelector(".__navlist");
     menu.classList.toggle("show-small");
   }
+
+  //when the page is done loading hide the loader
+window.addEventListener('load', function() {
+  var loader = document.querySelector('.loader');
+  setTimeout(function() {
+    loader.classList.add('hide');
+  }, 2000); // 2000 milliseconds = 3 seconds before showing content
+});
